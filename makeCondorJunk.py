c7c12841886c770b7945b5ec7b2fe9c8e76a1922
Jacob Jackson <jacob_jackson@brown.edu>
2018-07-20 06:40:34 -0500

    updating condorJunk default queue value

--- makeCondorJunk.py
+++ makeCondorJunk.py
@@ -9,7 +9,7 @@ parser.add_option("-d", "--doDirSplitting", dest = "doDirSplitting",
                   action="store_true", default=False,
                   help = "do the stuff in makeAllSmallerDirs.py")
 parser.add_option("-m", "--magicNumber", dest = "magicNumber", 
-                  type = int, default=4,
+                  type = int, default=1,
                   help = "the magic number corresponding to the jdl's 'queue' setting")
 (options, args) = parser.parse_args()
 
